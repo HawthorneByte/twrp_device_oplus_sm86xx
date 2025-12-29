117a5b58d99d99783f63aed9d2c504fa4917d148
Wanderlust <easts@vip.qq.com>
2025-12-29 18:34:24 +0800

    sm86xx: Initial skeleton tree
    Signed-off-by: Hawthorne <easts@vip.qq.com>

---
+++ twrp_sm86xx.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/oplus/sm86xx
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+## Device identifier
+PRODUCT_DEVICE  := sm86xx
+PRODUCT_NAME    := twrp_sm86xx
+PRODUCT_BRAND   := oplus
+
+# Theme
+TW_STATUS_ICONS_ALIGN   := center
